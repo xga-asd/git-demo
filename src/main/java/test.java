e5f0243f1d85c1b68dcea21b289fdb952cd3ddf8
xga <123456@163.com>
2021-08-27 19:04:42 +0800

    hot-fix

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello atguigu");
         System.out.println("hello word");
+        System.out.println("hot-fix");
     }
 }
